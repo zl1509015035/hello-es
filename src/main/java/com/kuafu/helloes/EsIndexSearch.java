c966b618220c069f7431f72508048f31ff695542
lei.zhu <lei.zhu@xforwardai.com>
2023-04-27 18:31:25 +0800

    feat: 1、查询索引

---
+++ src/main/java/com/kuafu/helloes/EsIndexSearch.java
@@ -0,0 +1,37 @@
+package com.kuafu.helloes;
+
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.cluster.metadata.MappingMetadata;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class EsIndexSearch {
+
+
+    public static void main(String[] args) throws IOException {
+        //创建ES客户端
+        RestHighLevelClient esClient = EsClient.getHighLevelClient();
+
+        //查询索引
+        GetIndexRequest request = new GetIndexRequest("user");
+
+        GetIndexResponse getIndexResponse = esClient.indices().get(request, RequestOptions.DEFAULT);
+        //别名
+        System.out.println(getIndexResponse.getAliases());
+        //映射
+        Map<String, MappingMetadata> mappings = getIndexResponse.getMappings();
+
+        System.out.println(getIndexResponse.getMappings());
+        //配置分片、副本
+        System.out.println(getIndexResponse.getSettings());
+        //
+        //关闭ES客户端
+        esClient.close();
+    }
+}
