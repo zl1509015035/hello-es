9aa48bc9199bc15ba5184d45483eb8373a507380
lei.zhu <lei.zhu@xforwardai.com>
2023-04-28 11:06:54 +0800

    feat: 1、文档批量新增 增加多条数据

--- src/main/java/com/kuafu/helloes/document/batch/EsDocInsertBatch.java
+++ src/main/java/com/kuafu/helloes/document/batch/EsDocInsertBatch.java
@@ -22,7 +22,15 @@ public class EsDocInsertBatch {
 
         bulkRequest.add(initRequest("1001",new User("张三","男",18)));
         bulkRequest.add(initRequest("1002",new User("李四","女",19)));
-        bulkRequest.add(initRequest("1003",new User("王五","男",25)));
+        bulkRequest.add(initRequest("1003",new User("王五3","男",25)));
+        bulkRequest.add(initRequest("1004",new User("王五4","女",23)));
+        bulkRequest.add(initRequest("1005",new User("王五5","男",32)));
+        bulkRequest.add(initRequest("1006",new User("王五6","女",27)));
+        bulkRequest.add(initRequest("1007",new User("王五7","女",29)));
+        bulkRequest.add(initRequest("1008",new User("王五8","男",32)));
+        bulkRequest.add(initRequest("1009",new User("王五9","男",35)));
+        bulkRequest.add(initRequest("10010",new User("王五10","女",37)));
+        bulkRequest.add(initRequest("10011",new User("王五11","女",19)));
         BulkResponse response = esClient.bulk(bulkRequest, RequestOptions.DEFAULT);
 
         System.out.println(response);
